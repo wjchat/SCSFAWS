07f31c71638ce4a41a1cacee5e5f15aac538c073
Will Chatterson <wjchat@umich.edu>
2020-04-30 14:47:46 -0400

    first commit

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,52 @@
+require("dotenv").config({  
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+pathPrefix: "/scsf",
+  siteMetadata: {
+    title: `So Close So Far`,
+    description: `Stay close.`,
+    author: `Jed & Will w/ Matte`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.API_URL || "http://localhost:1337",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "user-uploads",
+        ],
+        singleTypes: ['front-page-video'],
+        queryLimit: 1000,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/Matte.svg`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+     `gatsby-plugin-offline`,
+  ],
+}
